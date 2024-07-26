04fbffe50dae7aa001474bdd0ffaa7c801e0649e
Manish <manish.jsx@gmail.com>
2024-07-26 20:44:49 +0530

    gitignore added

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,5 @@
 
 
-# from django.urls import path
-
-# from wagtail.model import serve
-
-# urlpatterns = [
-#     path('', serve, name='blog_index'),  # Use Wagtail's `serve` view
-#     path('<slug:slug>/', serve, name='blog_page'),  # Use Wagtail's `serve` view
-# ]
-
 # blog/urls.py
 from django.urls import path
 from wagtail.views import serve
